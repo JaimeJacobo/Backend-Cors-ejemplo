15cb4c5321e31c7551324c00ef5811fd830c7969
Jaime Jacobo <jaimejacobordv@gmail.com>
2020-04-13 15:57:22 +0200

    prueba

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ const PORT = process.env.PORT || 3000
 const app = express();
 app.set('view engine', 'hbs');
 
+const allowedOrigins = ['localhost:3000'];
+
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 require('dotenv').config();
 
 
